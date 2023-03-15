f91eabd47eb27677de4963421fb909df3c5694e1
vietth <anhxtanhwp@gmail.com>
2023-03-15 12:00:33 +0700

    update: example

--- example/example.ts
+++ example/example.ts
@@ -2,7 +2,6 @@ import { Address } from "@multiversx/sdk-core/out";
 import { ProxyNetworkProvider } from "@multiversx/sdk-network-providers/out";
 import { ASHSWAP_CONFIG } from "../src/const/ashswapConfig";
 import { ContractManager } from '../src/helper/contracts';
-import { queryPoolContract } from "../src/helper/contracts/pool";
 import { MVXProxyNetworkAddress } from "../src/helper/proxy/util";
 import { ChainId } from "../src/helper/token/token";
 import BigNumber from "bignumber.js";
@@ -13,19 +12,21 @@ async function example() {
 
     const proxy = new ProxyNetworkProvider(MVXProxyNetworkAddress.Mainnet)
 
+    // FeeDistributorContract
     const fdContract = ContractManager.getFeeDistributorContract(
         ASHSWAP_CONFIG.dappContractMainnet.feeDistributor
     ).onChain(ChainId.Mainnet)
     .onProxy(proxy);
 
-
-    const tx = await fdContract.claim(new Address(Address.Zero().bech32()));
+    const tx = await fdContract.claim(Address.Zero());
     console.log("-->" + tx);
 
+    // VotingEscrowContract
+    const veContract = ContractManager.getVotingEscrowContract(
+        ASHSWAP_CONFIG.dappContractMainnet.voteEscrowedContract
+    );
+    const withdrawTx = await veContract.withdraw();
+    console.log("-->" + withdrawTx);
 
-    const contract = queryPoolContract.getAmountOutMaiarPool("", "", new BigNumber(0));
-}
 
-export function add(a: number, b: number): number {
-    return a + b;
 }
